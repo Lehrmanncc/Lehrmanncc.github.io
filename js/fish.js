eb5974e5d888efaf3d2cdbaec97e1b694c5cb743
Lehrmanncc <793478822@qq.com>
2020-11-19 21:52:42 +0800

    Site updated: 2020-11-19 21:52:42

---
+++ js/fish.js
@@ -0,0 +1,6 @@
+var xiaokang = new xkTool();
+xiaokang.footFish();
+
+$(document).ready(function(e) {
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> Lehrmann');
+});
